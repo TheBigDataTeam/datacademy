c60b815e3eb21dd45c5d81698f22760ad2dc2311
Serj1c <sergej.a.isaev@gmail.com>
2021-03-02 22:11:17 +0300

    DATA-12 btn design fixed+hover styles added

--- app/src/components/pages/Landing/Buttons/Buttons.tsx
+++ app/src/components/pages/Landing/Buttons/Buttons.tsx
@@ -3,16 +3,16 @@ import { Link } from 'react-router-dom';
 import { Button, Text } from '../../../ui';
 import styles from './Buttons.module.css';
 
-export const Buttons:React.FunctionComponent = () => {
+export const Buttons:React.FunctionComponent = (): JSX.Element => {
     return (
         <div className={styles.root}>
             <Link to="/auth/signup" className={styles.root_link}>
-                <Button design="default">
-                    <Text size="l" color="inverted">Sign Up</Text>
+                <Button design='secondary'>
+                    <Text size="l">Sign Up</Text>
                 </Button>
             </Link>
             <Link to="/auth/login" className={styles.root_link}>
-                <Button design="default">
+                <Button design='primary'>
                     <Text size="l" color="inverted">Sign In</Text>
                 </Button>
             </Link>
