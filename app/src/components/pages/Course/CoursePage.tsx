4ffb7cac9cf69a1c720a56562182f35c94bb1e25
Serj1c <sergej.a.isaev@gmail.com>
2021-05-09 20:27:16 +0300

    DATA-17 author and course models

--- app/src/components/pages/Course/CoursePage.tsx
+++ app/src/components/pages/Course/CoursePage.tsx
@@ -3,48 +3,49 @@ import { Header, Footer } from 'components/common';
 import { PageLayout } from 'components/layouts';
 import { Grid, Paragraph } from 'components/ui';
 import { AuthorSection, SyllabusSection, BenefitsSection, TechStackSection } from './components';
-import axios from 'axios';
+import { Author, Course } from 'models';
+import axios, { AxiosResponse } from 'axios';
 
 export const CoursePage: React.FunctionComponent = (): JSX.Element => {
 
-    /* TODO TypeScript */
-
-    const [courses, setCourses] = useState(null);
-    const [author, setAuthor] = useState(null);
+    const [course, setCourse] = useState<Course>(null);
+    const [author, setAuthor] = useState<Author>(null);
 
     /* TODO URLs to be dynamic */
 
     useEffect(() => {
         const fetchData = async (): Promise<void> => {
-            const result = await axios.get("http://localhost:3100/courses/1")
-            setCourses(result)
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            const result: AxiosResponse<any> = await axios.get("http://localhost:3100/courses/1")
+            setCourse(result.data)
         }
         fetchData();
     }, []);
 
     useEffect(() => {
         const fetchAuthor = async ():Promise<void> => {
-            const result = await axios.get("http://localhost:3100/authors/1")
-            setAuthor(result)
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            const result: AxiosResponse<any> = await axios.get("http://localhost:3100/authors/1")
+            setAuthor(result.data)
         }
         fetchAuthor();
     }, []);
 
     return (
         <PageLayout header={<Header />} footer={<Footer />} topOffset>
-            {courses ? 
+            {course ? 
             <>
                 <Grid.Row>
                     <Grid.Col>
-                        <Paragraph size="xxl">{courses.data.title}</Paragraph>
+                        <Paragraph size="xxl">{course.title}</Paragraph>
                     </Grid.Col>
                 </Grid.Row>
                 <AuthorSection author={author}/>
                 <BenefitsSection />
-                <SyllabusSection syllabus={courses.data.syllabus}/>
-                <TechStackSection techstack={courses.data.techstack}/>
+                <SyllabusSection syllabus={course.syllabus}/>
+                <TechStackSection techstack={course.techstack}/>
             </>
-            : <h2>Loading</h2>}
+            : <h2>Loading...</h2>}
         </PageLayout>
     ) 
 }
---
+++ app/src/models/Course.ts
@@ -0,0 +1,13 @@
+export type Course = {
+    id: number
+    title: string
+    description: string
+    theme: string
+    author: string
+    techstack: Array<string>
+    syllabus: Array<string>
+    duration: string
+    difficulty: string
+    createdOn: string
+    updatedOn: string
+}
