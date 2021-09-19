2fd9c880c769f679c2e81c7e661335140762e8d4
Serj1c <sergej.a.isaev@gmail.com>
2021-09-19 16:05:30 +0300

    DATA-27 insert grids in dashboard page

--- app/src/components/pages/Dashboard/components/SubscribedSection/SubscribedSection.tsx
+++ app/src/components/pages/Dashboard/components/SubscribedSection/SubscribedSection.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Grid } from 'components/ui'
 
 export const SubscribedSection: React.FunctionComponent = (): JSX.Element => {
     return (
-        <div>
-            <h2>You are not subscribed to anything</h2>
-        </div>
+        <>
+            <Grid.Row>
+                <Grid.Col>
+                    <h2>You are not subscribed to anything</h2>
+                </Grid.Col>
+            </Grid.Row>
+        </>
     )
 }
