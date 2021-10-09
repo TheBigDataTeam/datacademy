1b05be5f874e998184d45b955c0f13d4f0b74bf2
Serj1c <sergej.a.isaev@gmail.com>
2021-10-09 13:05:59 +0300

    DATA-24 show username in the header

--- app/src/components/common/Header/Header.tsx
+++ app/src/components/common/Header/Header.tsx
@@ -13,6 +13,7 @@ interface Props {
 export const Header: React.FunctionComponent<Props> = ({ inverted }): JSX.Element => {
 
 	const isLoaded = useSelector((state: AppStateType) => state.userAuth.isLoaded)
+	const user = useSelector((state: AppStateType) => state.userAuth.user)
 
 	return (
 		<div className={styles.root}>
@@ -20,14 +21,18 @@ export const Header: React.FunctionComponent<Props> = ({ inverted }): JSX.Elemen
 				<Logo inverted={inverted} />
 			</div>
 			{ inverted ? <Menu inverted /> : <Menu /> }
-			{ isLoaded ?
-			<Link to="/auth/logout" className={styles.right}>
-				{ inverted ? <Logout inverted /> : <Logout /> }
-			</Link>
-			: <Link to="/auth/login" className={styles.right}>
-			{ inverted ? <Login inverted /> : <Login /> }
-			</Link>
-			}
+			<div className={styles.right}>
+				{ isLoaded ? <span className={styles.name}>{user.name}</span> : <span className={styles.name}>Guest</span>}
+				{ isLoaded ?
+				<Link to="/auth/logout" className={styles.link}>
+					{ inverted ? <Logout inverted /> : <Logout /> }
+				</Link>
+				: 
+				<Link to="/auth/login">
+					{ inverted ? <Login inverted /> : <Login /> }
+				</Link>
+				}
+			</div>
 		</div>
 	)
 }
