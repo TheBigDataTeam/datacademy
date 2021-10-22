5a66f3aa44f67dfe0a3ad58be13463cb935cb96d
Serj1c <sergej.a.isaev@gmail.com>
2021-10-22 21:32:10 +0300

    DATA-44 create a blank for a 404 page

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,11 +1,10 @@
 import React, { useEffect } from 'react'
 import './App.module.css'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { LandingPage, SignUpPage, LoginPage, LogoutPage,
 		ForgetPassPage, CatalogPage, AuthorsPage, 
 		ProjectInfoPage, PricingPage, PaymentPage,
-		CoursePage, AuthorPage, DashboardPage
-} from 'components/pages'
+		CoursePage, AuthorPage, DashboardPage, Page404 } from 'components/pages'
 import { AddAuthorPage, AdminDashboardPage } from 'components/admin'
 import { useDispatch } from 'react-redux'
 import { fetchUserLogin } from 'redux/user_auth/userAuthActions'
@@ -24,23 +23,26 @@ export const App: React.FunctionComponent = (): JSX.Element => {
 	
 	return (
 		<Router>
-			<Route exact path="/" component={LandingPage}/>
-			<Route path="/auth/signup" component={SignUpPage}/>
-			<Route path="/auth/login" component={LoginPage}/>
-			<Route path="/auth/logout" component={LogoutPage}/>
-			<Route path="/auth/forget" component={ForgetPassPage}/>
-			<Route exact path="/courses" component={CatalogPage}/>
-			<Route path="/courses/:id" component={CoursePage}/>
-			<Route exact path="/authors" component={AuthorsPage}/>
-			<Route path="/authors/:id" component={AuthorPage}/>
-			<Route path="/pricing" component={PricingPage}/>
-			<Route path="/project" component={ProjectInfoPage}/>
-			<Route path="/payment" component={PaymentPage}/>
-			<Route path="/dashboard" component={DashboardPage}/>
-			{/* Admin Section */}
-			<Route path="/admin/add/course" />
-			<Route path="/admin/add/author" component={AddAuthorPage}/>
-			<Route path="/admin/dashboard" component={AdminDashboardPage}/>
+			<Switch>
+				<Route exact path="/" component={LandingPage}/>
+				<Route path="/auth/signup" component={SignUpPage}/>
+				<Route path="/auth/login" component={LoginPage}/>
+				<Route path="/auth/logout" component={LogoutPage}/>
+				<Route path="/auth/forget" component={ForgetPassPage}/>
+				<Route exact path="/courses" component={CatalogPage}/>
+				<Route path="/courses/:id" component={CoursePage}/>
+				<Route exact path="/authors" component={AuthorsPage}/>
+				<Route path="/authors/:id" component={AuthorPage}/>
+				<Route path="/pricing" component={PricingPage}/>
+				<Route path="/project" component={ProjectInfoPage}/>
+				<Route path="/payment" component={PaymentPage}/>
+				<Route path="/dashboard" component={DashboardPage}/>
+				<Route component={Page404}/>
+				{/* Admin Section */}
+				<Route path="/admin/add/course" />
+				<Route path="/admin/add/author" component={AddAuthorPage}/>
+				<Route path="/admin/dashboard" component={AdminDashboardPage}/>
+			</Switch>
 		</Router>
 	)
 }
---
+++ app/src/components/pages/404/Page404.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Page404: React.FunctionComponent = (): JSX.Element => {
+    return (
+        <div>
+            <h2>It seems that we have not found what you were looking for</h2>
+        </div>
+    )
+}
