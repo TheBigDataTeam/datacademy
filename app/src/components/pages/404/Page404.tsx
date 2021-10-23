0e57bf04e860885a17613ca0a98364fb6e095f3f
Serj1c <sergej.a.isaev@gmail.com>
2021-10-23 23:49:14 +0300

    DATA-44 finish 404-page styling

--- app/src/components/pages/404/Page404.tsx
+++ app/src/components/pages/404/Page404.tsx
@@ -4,6 +4,7 @@ import { PageLayout } from 'components/layouts'
 import { Header, Footer } from 'components/common'
 import { Heading, Grid, Paragraph } from 'components/ui'
 import { useDocTitle } from 'components/hooks'
+import styles from './Page404.module.css'
 
 export const Page404: React.FunctionComponent = (): JSX.Element => {
 
@@ -12,10 +13,12 @@ export const Page404: React.FunctionComponent = (): JSX.Element => {
     return (
         <PageLayout header={<Header />} footer={<Footer />} topOffset>
             <Grid.Row>
-                <Grid.Col>
-                    <Heading size="xxl">404</Heading>
-                    <Paragraph size="l">It seems that we have not found what you were looking for</Paragraph>
-                    <Paragraph>You may go to <Link to="/">home page</Link> or check our <Link to="/courses">courses</Link></Paragraph>
+                <Grid.Col align="center">
+                    <div className={styles.root}>
+                        <Heading size="xxl" align="center">404</Heading>
+                        <Paragraph size="l" align="center">It seems that we have not found what you were looking for</Paragraph>
+                        <Paragraph align="center">You may go to <Link to="/">home page</Link> or check our <Link to="/courses">courses</Link></Paragraph>
+                    </div> 
                 </Grid.Col>
             </Grid.Row>
         </PageLayout>
