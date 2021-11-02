fa0c657c647cee53d8dff953c552b77932d77d78
Serj1c <sergej.a.isaev@gmail.com>
2021-11-02 21:01:31 +0300

    DATA-45 add aws-auth.yaml for admin cluster user

--- app/src/constants/common.ts
+++ app/src/constants/common.ts
@@ -2,7 +2,7 @@ export const HOMEPAGE_URL = '/'
 export const USER_HOMEPAGE_URL = '/dashboard'
 export const ADMIN_HOMEPAGE_URL = '/admin/dashboard'
 export const TITLE_PREFIX = 'DataLearn | '
-export const BASE_URL = 'http://localhost:3100'
+export const BASE_URL = 'http://api:3100'
 
 export const EMAIL = 'TBD'
 export const START_YEAR = 2021
